67a6617830d6b592e4aefba01c7e1b58cda3b9e6
Nikita Murzin <r0s0m4xa@gmail.com>
2024-01-28 18:01:33 +0100

    Add index endpoint

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\DTO\CreateTeamDTO;
 use App\Entity\Team;
+use App\Repository\TeamRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -13,7 +14,7 @@ use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 
 class TeamController extends AbstractController
 {
-    #[Route('/team', name: 'create_team')]
+    #[Route('/team', name: 'create_team', methods: ['POST'])]
     public function createTeam(
         #[MapRequestPayload] CreateTeamDTO $createTeamDTO,
         EntityManagerInterface $entityManager
@@ -28,6 +29,16 @@ class TeamController extends AbstractController
         return $this->json(
             $team,
             Response::HTTP_CREATED
+        );
+    }
+
+    #[Route('/team', name: 'get_teams', methods: ['GET'])]
+    public function index(
+        TeamRepository $teamRepository
+    ): JsonResponse
+    {
+        return $this->json(
+            $teamRepository->findAll()
         );
     }
 }
